f7ee6d60a07a612139b5d72771223c24bc272043
Ananth Garg <gargananth1998@gmail.com>
2018-09-08 05:55:02 +0530

    Initial commit

---
+++ app/src/main/java/com/example/ananthgarg/peoplecounter/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.ananthgarg.peoplecounter;
+
+import android.os.Bundle;
+import android.support.wearable.activity.WearableActivity;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends WearableActivity {
+
+    private TextView mTextView;
+    int count=0;
+public void plusone(View view)
+{
+    count++;
+    mTextView.setText(Integer.toString(count));
+}
+public void reset(View view)
+{
+    count=0;
+    mTextView.setText(Integer.toString(count));
+}
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mTextView = (TextView) findViewById(R.id.text);
+
+        // Enables Always-on
+        setAmbientEnabled();
+    }
+}
